52db69da5f5a5bdac1ec3322fc213201423a7fa7
Abhishek <abhishekkumar200218@gmail.com>
2026-05-19 00:08:18 +0530

    Happy Number

---
+++ Basic/Happy Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int temp=n;
+        while(temp!=1 && temp != 4){
+            int num = temp;
+            int sq = 0;
+            while (num > 0) {
+                int a = num % 10;   // get last digit
+                sq = a * a + sq;  // add square of digit
+                num = num / 10;       // remove last digit
+            }
+            temp=sq;
+        }
+        if(temp==1){
+            return true;
+        }
+        return false;
+    }
+};
