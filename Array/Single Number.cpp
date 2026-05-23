1f0be74df9cfc15e38a0b4879d451989f82210ff
Abhishek <abhishekkumar200218@gmail.com>
2026-05-23 23:31:47 +0530

    Single Number

---
+++ Array/Single Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+        int k=0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    k++;
+                }
+            }
+            if(k==1){
+                return nums[i];
+            }
+        }
+        return {};
+    }
+};
