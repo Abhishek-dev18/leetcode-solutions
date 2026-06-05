215b2b80852e599f82d2fb76461f881f9bfdcdf9
Abhishek <abhishekkumar200218@gmail.com>
2026-06-05 22:03:59 +0530

    Max Consecutive Ones

--- Array/Max Consecutive Ones.cpp
+++ Array/Max Consecutive Ones.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int n=nums.size(),max=0,tot=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){  
             if(nums[i]==1){
                 tot=tot+1;  
                 if(tot>max){
