91eb545a18475f9a0a099db7603a73af075788f7
Abhishek <abhishekkumar200218@gmail.com>
2026-06-23 22:39:56 +0530

    Best Time to Buy and Sell Stock

--- Array/Best Time to Buy and Sell Stock.cpp
+++ Array/Best Time to Buy and Sell Stock.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices){
         int n=prices.size();
-        int mn = prices[0];
+        int mn = prices[0]; 
         int profit = 0;
 
         for(int i=1;i<n;i++) {
