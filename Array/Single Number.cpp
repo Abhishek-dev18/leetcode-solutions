9c94abdb4adb4ee674c68860734cdea0049aba57
Abhishek <113304205+Abhishek-dev18@users.noreply.github.com>
2026-06-24 23:05:33 +0530

    Single Number

--- Array/Single Number.cpp
+++ Array/Single Number.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<n;i++){
         int k=0;
             for(int j=0;j<n;j++){ 
-                if(nums[i]==nums[j]){
+                if(nums[i]==nums[j]){ 
                     k++;
                 }
             } 
